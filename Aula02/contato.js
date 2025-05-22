00ea8468a8771eff0753b5fd4cb3fc253676bc0e
Gabriel De Avellar <93794192+gabrieldeavellar@users.noreply.github.com>
2025-05-22 12:01:02 -0300

    Add files via upload

--- Aula02/contato.js
+++ Aula02/contato.js
@@ -1,3 +1,16 @@
-function enviar(){
-    console.log("to funcionando")
+function enviar() {
+    document.getElementById("nome").style.border = "none"
+    document.getElementById("msg").style.border = "none"
+    const nome = document.getElementById("nome").value
+    const assunto = document.getElementById("msg").value
+ 
+    if (!nome || !assunto) {
+        document.getElementById("nome").style.border = "3px solid red"
+        document.getElementById("msg").style.border = "3px solid red"
+        return
+    }
+ 
+    const mensagem = `Gostaria de entrar em contato! \n\nNome: ${nome} \nAssunto: ${assunto}`
+    const msg = encodeURIComponent(mensagem)
+    window.open(`https://wa.me/5541999999999?text=${msg}`, "_blank")
 }
